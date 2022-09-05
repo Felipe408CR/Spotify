3ead826b98cedc4e368e959e82b1a5e02a0b618f
206 <206@academicos.cesde.edu.co>
2022-09-05 12:12:57 -0500

    Se inicia TOKEN automatico

--- src/helpers/datosPeticionGet.js
+++ src/helpers/datosPeticionGet.js
@@ -4,7 +4,9 @@ const ID_ARTISTA="0NGAZxHanS9e0iNHpR8f2W"
 
 export const URI= `https://api.spotify.com/v1/artists/${ID_ARTISTA}/top-tracks?market=US`
 
-const TOKEN= "Bearer BQCYS1ReZVjRnNUkVGxupi6Z5abm1gmpQxHFB4pgKhVvZG1igSFLzupDBa-wH_ZQ9Q5b6gtBw6M5KIfntBMiIkHsj5e0394y-AfhSq0WIvVyS9cdx30SV_anU2z_leYi3UDXyzvf6yn_Sl4QEDoTTEXPkK5rQObnXM3ZOnv2i-wBMU-I1liKXLg4wpXP1qE"
+// Llamar al servicio de token
+
+const TOKEN= "Bearer BQBMYBVpfBFQbfVd6G_5Ujh798hHrywQyo6hJjvbA5KNl82sH1Lj-3S5Mq26O9XiSyL9ZjDFIPSirqRuZ47aYi4tE0DPHWlkzgb1za1i6IYr47BLCvL0jkw6Mbg1bGucX6bnJbVkKCiXmSxKRaFWKDu5YAB0UnX-WtsMrImHG95mLPS8cE4vPxZGaUdnZ24"
 
 // 2. Configurar la petición
 
--- src/services/generarToken.js
+++ src/services/generarToken.js
@@ -8,16 +8,39 @@ const URI = "https://accounts.spotify.com/api/token"
 
 const DATO1 = "grant_type=client_credentials"
 const DATO2 = "client_id=04f68e9473d34215876bd5798e02d974"
-const DATO3 = "client_secret=f600bf608f8f4f1298925912cfff8e60"
+const DATO3 = "client_secret=961798f5793b4acc81ddc563a4bd46a5"
 
 // PARAMETROS PETICION 
 
 const PETICION = {
 
     method: "POST",
-    headers: {},
+    headers: {"Content-Type":"application/x-www-form-urlencoded"},
     body: DATO1+"&"+DATO2+"&"+DATO3
 
 }
 
-// FETCH
+// // FETCH
+
+// fetch(URI,PETICION)
+
+// .then(function(respuesta){
+//     return respuesta.json()
+// })
+
+// .then(function(respuesta){
+//     console.log(respuesta)
+// })
+
+// .catch(function(respuesta){
+//     console.log(respuesta)
+// })
+
+// ASYNC AWAIT
+
+async function obtenerToken(){
+    let respuesta=await fetch(URI,PETICION)
+    return await respuesta.json()
+}
+
+console.log(await obtenerToken())
