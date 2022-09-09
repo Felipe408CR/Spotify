fa3db3599bb9aed18a96c1074fac7cf4cf50c908
Julian Rios <Jarc2802@gmail.com>
2022-09-09 10:56:52 -0500

    Se inicia funcion TOKEN

--- src/helpers/datosPeticionGet.js
+++ src/helpers/datosPeticionGet.js
@@ -6,7 +6,7 @@ export const URI= `https://api.spotify.com/v1/artists/${ID_ARTISTA}/top-tracks?m
 
 // Llamar al servicio de token
 
-const TOKEN= "Bearer BQBMYBVpfBFQbfVd6G_5Ujh798hHrywQyo6hJjvbA5KNl82sH1Lj-3S5Mq26O9XiSyL9ZjDFIPSirqRuZ47aYi4tE0DPHWlkzgb1za1i6IYr47BLCvL0jkw6Mbg1bGucX6bnJbVkKCiXmSxKRaFWKDu5YAB0UnX-WtsMrImHG95mLPS8cE4vPxZGaUdnZ24"
+const TOKEN= "Bearer BQA2t6kPfFi2w0x0o3svpjO1gtbV7WKmS7aULAAb56fHhzSQe7vhWsOoDOEViJbgGTcyMaaPrISWEEm9zaQrltG50S3I4btweu-ob9S1HJq4XH-DP_qrgjmCoVtmKAoRGUzbmh-gxFFfgu-8_PLI4t_KJy72nKvwwjCNOnBXGk4BAAU4GCjrDWqrwVfA_6s" es un conjunto de herramientas que facilitan 
 
 // 2. Configurar la petición
 
